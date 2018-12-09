8f34a4ef51dbd8e3f894af9f931e989ba1974cf0
Samuel Catley <scp94373@gmail.com>
2018-12-09 11:57:54 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+  //Logging to console when webpage has been requested
+  console.log('request to /');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/time', (req, res) => {
+  //Logging to console when webpage has been requested
+  console.log('request to /');
+  const time = new Date().getTime();
+  res.send(time.toString());
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
