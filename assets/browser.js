a602f8ea4a7807bd300af88fe993b34a1a0fee61
Samuel Catley <scp94373@gmail.com>
2019-01-13 13:54:46 +0000

    Update130119

--- assets/browser.js
+++ assets/browser.js
@@ -17,7 +17,7 @@ if (playerChoice !== 'X' || playerChoice !== 'O') {
   alert('Let the game begin!');
 }
 */
-/*
+//TODO: Put a check if player has already chosen X or O and tries to click the other option (x or o)
 var choiceX = 0;
 var choiceO = 0;
 var classButtonX = document.getElementById("buttonX");
@@ -26,7 +26,8 @@ var classButtonO = document.getElementById("buttonO");
 
 function chooseX() {
   if(choiceX === 0) {
-    choiceX = 1;
+  choiceX = 1;
+  _playerInput = "X";
     classButtonX.style.visibility = "hidden";
     alert("You have chosen X");
     //var hidden =
@@ -35,13 +36,15 @@ function chooseX() {
   } else {
     alert("X has already been chosen, you will be O");
     choiceO = 1;
+    _playerInput = "O";
     classButtonO.style.visibility = "hidden";
   }
-};
+}
 
 function chooseO() {
   if(choiceO === 0) {
-    choiceO = 1;
+  choiceO = 1;
+    _playerInput = "O";
     classButtonO.style.visibility = "hidden";
     alert("You have chosen O");
     //Button now visible after pressing if O was chosen
@@ -49,15 +52,16 @@ function chooseO() {
   } else {
     alert("O has already been chosen, you will be X");
     choiceX = 1;
+    _playerInput = "X";
     classButtonX.style.visibility = "hidden";
   }
-};
-*/
+}
 
 
+/*
 function playerChoice (checkInput) {
 _playerInput = prompt("Please choose your player", "X or O");
-};
+}
 //TODO - Currently _playerInput check does not work for checking correctly.
 function checkInput(_playerInput) {
 if (_playerInput !== "X") {
@@ -68,8 +72,8 @@ if (_playerInput !== "X") {
   } else {
   alert('You have picked ' + _playerInput + " Let the game begin!");
   }
-};
-
+}
+*/
 const board = document.getElementById('board');
 /*
 Function getBoard will call board API endpoint and retrieve the board data.
@@ -84,24 +88,24 @@ function getBoard() {
       //generate table marker into the HTML page.
       board.innerHTML = boardHTML;
     }
-  }
+  };
   xhttp.open("GET", "/api/board", true);
   xhttp.send();
-};
+}
 /*
 This function will take board data and generate a table from the data.
 */
-function generateBoard(boardData) {
-  let HTML = '<table>'
-  boardData.forEach( function(row, rowIndex) {
-    HTML += '<tr>'
-    row.forEach( function(column, columnIndex) {
-      HTML += `<td onclick="handleClick(${rowIndex}, ${columnIndex})">${column}</td>`
+function generateBoard(board) {
+  var HTML = '<table>';
+  [board].forEach( function(row, rowIndex) {
+    HTML += '<tr>';
+    [row].forEach( function(column, columnIndex) {
+      HTML += `<td onclick="handleClick(${rowIndex}, ${columnIndex})">${column}</td>`;
 
-    })
-    HTML += '</tr>'
-  })
-  HTML += '</table>'
+    });
+    HTML += '</tr>';
+  });
+  HTML += '</table>';
   return HTML;
 }
 
@@ -115,9 +119,9 @@ function playerMove(rowIndex, columnIndex) {
   xhttp.open("GET", `/api/move?row=${rowIndex}&column=${columnIndex}&player=${_playerInput}`, true);
   xhttp.send();
 
-};
-playerChoice();
-checkInput(_playerInput);
+}
+
+//checkInput(_playerInput);
 /*
 Calling the board function
 */
